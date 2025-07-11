c9ae5a318f4a9031d6b5e0bc071f83492bfd6e2e
huid <1732122592@qq.com>
2025-07-11 21:34:12 +0800

    Refactor interpreter structure: rename Store to Memory and update methods
    - Renamed Store to Memory for clarity and updated associated methods.
    - Changed internal representation from codepoints to cells and idx to pointer.
    - Updated method names to reflect new structure (e.g., increment_cell, move_pointer_right).
    - Refactored parsing logic to use new Memory structure.
    - Adjusted execution logic to work with the new Memory representation.

--- src/main.rs
+++ src/main.rs
@@ -32,41 +32,47 @@ impl Token {
     }
 }
 
-struct Store {
-    codepoints: Vec<u8>,
-    idx: usize,
+struct Memory {
+    cells: Vec<u8>,
+    pointer: usize,
     output_buffer: Vec<u8>,
 }
 
-impl Store {
+impl Memory {
     fn new() -> Self {
         Self {
-            codepoints: vec![0; 30000],
-            idx: 0,
+            cells: vec![0; 30000],
+            pointer: 0,
             output_buffer: Vec::with_capacity(64),
         }
     }
-    fn increment(&mut self, c: u32) {
-        self.codepoints[self.idx] = self.codepoints[self.idx].wrapping_add(c as u8);
+    #[inline(always)]
+    fn increment_cell(&mut self, c: u32) {
+        self.cells[self.pointer] = self.cells[self.pointer].wrapping_add(c as u8);
     }
-    fn decrement(&mut self, c: u32) {
-        self.codepoints[self.idx] = self.codepoints[self.idx].wrapping_sub(c as u8);
+    #[inline(always)]
+    fn decrement_cell(&mut self, c: u32) {
+        self.cells[self.pointer] = self.cells[self.pointer].wrapping_sub(c as u8);
     }
-    fn move_right(&mut self, c: u32) {
-        self.idx += c as usize;
+    #[inline(always)]
+    fn move_pointer_right(&mut self, c: u32) {
+        self.pointer += c as usize;
     }
-    fn move_left(&mut self, c: u32) {
-        if self.idx < 1 {
+    #[inline(always)]
+    fn move_pointer_left(&mut self, c: u32) {
+        if self.pointer < 1 {
             panic!("Pointer underflow: attempted to move left at index 0");
         }
-        self.idx -= c as usize;
+        self.pointer -= c as usize;
     }
-    fn print(&mut self) {
-        self.output_buffer.push(self.codepoints[self.idx]);
+    #[inline(always)]
+    fn output_cell(&mut self) {
+        self.output_buffer.push(self.cells[self.pointer]);
         if self.output_buffer.len() >= 64 {
             self.flush();
         }
     }
+    #[inline(always)]
     fn flush(&mut self) {
         if !self.output_buffer.is_empty() {
             std::io::stdout().write_all(&self.output_buffer).unwrap();
@@ -74,10 +80,11 @@ impl Store {
             self.output_buffer.clear();
         }
     }
-    fn read(&mut self) {
+    #[inline(always)]
+    fn input_cell(&mut self) {
         let mut buf = [0u8; 1];
         std::io::stdin().read_exact(&mut buf).unwrap();
-        self.codepoints[self.idx] = buf[0];
+        self.cells[self.pointer] = buf[0];
     }
 }
 
@@ -93,51 +100,51 @@ enum Expr {
     Output,
 }
 
-struct Code {
-    code: Vec<char>,
-    store: Store,
-    exprs: Vec<Expr>,
+struct Interpreter {
+    source: Vec<char>,
+    memory: Memory,
+    ast: Vec<Expr>,
 }
 
-impl Code {
-    fn new(code: Vec<char>, store: Store) -> Self {
+impl Interpreter {
+    fn new(source: Vec<char>, memory: Memory) -> Self {
         Self {
-            code,
-            store,
-            exprs: vec![],
+            source,
+            memory,
+            ast: vec![],
         }
     }
-    fn pre_parsing(&mut self) {
-        let mut track_stk: Vec<Vec<Expr>> = Vec::new();
-        let mut crt_exprs: Vec<Expr> = Vec::new();
+    fn parse(&mut self) {
+        let mut loop_stack: Vec<Vec<Expr>> = Vec::new();
+        let mut current_exprs: Vec<Expr> = Vec::new();
 
-        for (i, c) in self.code.iter().enumerate() {
+        for (i, c) in self.source.iter().enumerate() {
             match Token::from_char(*c) {
-                Token::Plus => match crt_exprs.last_mut() {
+                Token::Plus => match current_exprs.last_mut() {
                     Some(Expr::Increment(n)) => *n += 1,
-                    _ => crt_exprs.push(Expr::Increment(1)),
+                    _ => current_exprs.push(Expr::Increment(1)),
                 },
-                Token::Minus => match crt_exprs.last_mut() {
+                Token::Minus => match current_exprs.last_mut() {
                     Some(Expr::Decrement(n)) => *n += 1,
-                    _ => crt_exprs.push(Expr::Decrement(1)),
+                    _ => current_exprs.push(Expr::Decrement(1)),
                 },
-                Token::Right => match crt_exprs.last_mut() {
+                Token::Right => match current_exprs.last_mut() {
                     Some(Expr::MoveRight(n)) => *n += 1,
-                    _ => crt_exprs.push(Expr::MoveRight(1)),
+                    _ => current_exprs.push(Expr::MoveRight(1)),
                 },
-                Token::Left => match crt_exprs.last_mut() {
+                Token::Left => match current_exprs.last_mut() {
                     Some(Expr::MoveLeft(n)) => *n += 1,
-                    _ => crt_exprs.push(Expr::MoveLeft(1)),
+                    _ => current_exprs.push(Expr::MoveLeft(1)),
                 },
-                Token::Dot => crt_exprs.push(Expr::Output),
-                Token::Comma => crt_exprs.push(Expr::Input),
+                Token::Dot => current_exprs.push(Expr::Output),
+                Token::Comma => current_exprs.push(Expr::Input),
                 Token::BracketOpen => {
-                    track_stk.push(crt_exprs);
-                    crt_exprs = Vec::new();
+                    loop_stack.push(current_exprs);
+                    current_exprs = Vec::new();
                 }
                 Token::BracketClose => {
-                    let loop_exprs = crt_exprs;
-                    crt_exprs = track_stk
+                    let loop_exprs = current_exprs;
+                    current_exprs = loop_stack
                         .pop()
                         .unwrap_or_else(|| panic!("Unmatched closing bracket at {}", i));
                     let exps = if let [Expr::Decrement(_)] = loop_exprs[..] {
@@ -145,37 +152,37 @@ impl Code {
                     } else {
                         Expr::Loop { exprs: loop_exprs }
                     };
-                    crt_exprs.push(exps);
+                    current_exprs.push(exps);
                 }
                 Token::Ignore => {}
             }
         }
-        if !track_stk.is_empty() {
+        if !loop_stack.is_empty() {
             panic!("Unmatched opening bracket");
         }
-        self.exprs = crt_exprs;
+        self.ast = current_exprs;
     }
     fn run(&mut self) {
-        fn execute(exprs: &mut Vec<Expr>, store: &mut Store) {
+        fn execute(exprs: &mut Vec<Expr>, memory: &mut Memory) {
             for e in exprs.iter_mut() {
                 match e {
-                    Expr::Increment(count) => store.increment(*count),
-                    Expr::Decrement(count) => store.decrement(*count),
-                    Expr::MoveRight(count) => store.move_right(*count),
-                    Expr::MoveLeft(count) => store.move_left(*count),
-                    Expr::MakeZero => store.codepoints[store.idx] = 0,
-                    Expr::Output => store.print(),
-                    Expr::Input => store.read(),
+                    Expr::Increment(count) => memory.increment_cell(*count),
+                    Expr::Decrement(count) => memory.decrement_cell(*count),
+                    Expr::MoveRight(count) => memory.move_pointer_right(*count),
+                    Expr::MoveLeft(count) => memory.move_pointer_left(*count),
+                    Expr::MakeZero => memory.cells[memory.pointer] = 0,
+                    Expr::Output => memory.output_cell(),
+                    Expr::Input => memory.input_cell(),
                     Expr::Loop { exprs } => {
-                        while store.codepoints[store.idx] != 0 {
-                            execute(exprs, store)
+                        while memory.cells[memory.pointer] != 0 {
+                            execute(exprs, memory)
                         }
                     }
                 }
             }
         }
-        execute(&mut self.exprs, &mut self.store);
-        self.store.flush();
+        execute(&mut self.ast, &mut self.memory);
+        self.memory.flush();
     }
 }
 
@@ -183,8 +190,7 @@ fn main() {
     let filepath = env::args().nth(1).unwrap();
     let filename = env::current_dir().unwrap().join(filepath);
     let content = std::fs::read_to_string(filename).unwrap();
-    let mut code = Code::new(content.chars().collect(), Store::new());
-    code.pre_parsing();
-    // print!("{:#?}", code.exprs);
-    code.run();
+    let mut interpreter = Interpreter::new(content.chars().collect(), Memory::new());
+    interpreter.parse();
+    interpreter.run();
 }
